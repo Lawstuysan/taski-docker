722187846a00a00df17de9301c7423b9c27704d1
Alex Zheltov <lsswow69@gmail.com>
2024-10-04 14:54:58 +0300

    5

---
+++ backend/api/tests.py
@@ -0,0 +1,27 @@
+"""Tests for the Task API."""
+
+from http import HTTPStatus
+
+from django.test import Client, TestCase
+
+from api import models
+
+
+class TaskiAPITestCase(TestCase):
+    """Test case for Task API."""
+
+    def setUp(self):
+        """Set up client for guest user."""
+        self.guest_client = Client()
+
+    def test_list_exists(self):
+        """Проверка доступности списка задач."""
+        response = self.guest_client.get('/api/tasks/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_task_creation(self):
+        """Проверка создания задачи."""
+        data = {'title': 'Test', 'description': 'Test'}
+        response = self.guest_client.post('/api/tasks/', data=data)
+        self.assertEqual(response.status_code, HTTPStatus.CREATED)
+        self.assertTrue(models.Task.objects.filter(title='Test').exists())
